527d81c474b0dd99accaa7540b990bd2aa4020cf
ashish-tanwar <atashishtanwar@gmail.com>
2018-10-17 21:48:35 +0530

    4th commit

--- config1.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    consumer_key:         'tr3Rw1wP3uNBey1atgSKdh55E'
-  , consumer_secret:      'A8GR9sLOUVnpaKfhyrs0WSStNWYvCRqJkFnENuD0Q61PmCt0WP'
-  , access_token:         '1052059504748232704-hPkRI7uL8ZLQh1ADvHUt5uM0LYukN8'
-  , access_token_secret:  'W6fIeIWKBQ3OgekpCMKVX2FB6v9gZTfFddrMmTLpvdoy2'
-}
-
